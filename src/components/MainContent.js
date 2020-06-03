efd9b7b99ca4917f4cd9ed415be916d734f678d2
Max Lemieux <max.lemieux@gmail.com>
2020-06-03 11:29:33 -0700

    new components

---
+++ src/components/MainContent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function MainContent() {
+  return (
+    <div className='main-content'>
+      Main Content here: index, portfolio, contact
+    </div>
+  )
+}
