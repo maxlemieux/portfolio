b474e4899b4f2061af789bc29b266a0d48bd62c7
Max Lemieux <max.lemieux@gmail.com>
2020-12-11 09:50:33 -0800

    styling

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import MainContent from './components/MainContent';
 
-
 const App = () => {
   const [state, setState] = useState({
     page: 'index'
